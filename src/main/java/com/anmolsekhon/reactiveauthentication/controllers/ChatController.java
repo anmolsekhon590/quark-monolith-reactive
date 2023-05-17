50dc372fcd63e10739fda640f51d07594c15e4f3
Anmol Sekhon <anmolsekhon590@gmail.com>
2023-05-17 01:32:52 -0400

    changed endpoint and added send method

--- src/main/java/com/anmolsekhon/reactiveauthentication/controllers/ChatController.java
+++ src/main/java/com/anmolsekhon/reactiveauthentication/controllers/ChatController.java
@@ -1,15 +1,22 @@
 package com.anmolsekhon.reactiveauthentication.controllers;
 
+import com.anmolsekhon.reactiveauthentication.models.Chat;
+import com.anmolsekhon.reactiveauthentication.services.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/api/")
+@RequiredArgsConstructor
+@RequestMapping("v1/api/chat")
 public class ChatController {
+    private final ChatService chatService;
+
     @GetMapping
-    public void send(@AuthenticationPrincipal String username) {
-        System.out.println("");
+    public void send(@AuthenticationPrincipal String username,
+                     @RequestBody Chat chat) {
     }
 }
