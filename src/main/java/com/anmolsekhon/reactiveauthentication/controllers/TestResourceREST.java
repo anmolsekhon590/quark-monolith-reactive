215f44fd8d8a209d7fe4399e9bd77aabbef6ac3c
Anmol Sekhon <anmolsekhon590@gmail.com>
2023-05-18 02:21:45 -0400

    added @Deprecated over Test stuff

--- src/main/java/com/anmolsekhon/reactiveauthentication/controllers/TestResourceREST.java
+++ src/main/java/com/anmolsekhon/reactiveauthentication/controllers/TestResourceREST.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Deprecated
 @RestController
 public class TestResourceREST {
 
