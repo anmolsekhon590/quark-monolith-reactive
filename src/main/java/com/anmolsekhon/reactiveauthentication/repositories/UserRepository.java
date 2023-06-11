d98cce922fadb3009a7f17d2cd07ec2bb9054712
Anmol Sekhon <anmolsekhon590@gmail.com>
2023-06-10 21:06:50 -0400

    moved projection to repository

--- src/main/java/com/anmolsekhon/reactiveauthentication/repositories/UserRepository.java
+++ src/main/java/com/anmolsekhon/reactiveauthentication/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package com.anmolsekhon.reactiveauthentication.repositories;
 
 import com.anmolsekhon.reactiveauthentication.entities.User;
-import com.anmolsekhon.reactiveauthentication.repositories.projections.UserView;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
     Mono<User> findByUsername(String username);
     Mono<UserView> findFriendsByUsername(String username);
+    interface UserView {
+        List<String> getFriends();
+    }
 }
--- src/main/java/com/anmolsekhon/reactiveauthentication/repositories/projections/UserView.java
+++
@@ -1,7 +0,0 @@
-package com.anmolsekhon.reactiveauthentication.repositories.projections;
-
-import java.util.List;
-
-public interface UserView {
-    List<String> getFriends();
-}
