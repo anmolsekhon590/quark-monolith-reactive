566a12ca66a02a4a527180c0e6affb28b231d25f
Anmol Sekhon <anmolsekhon590@gmail.com>
2023-06-24 18:43:06 -0400

    fixed .bat script

--- build-update-image.bat
+++ build-update-image.bat
@@ -1,2 +1,7 @@
-.\gradlew bootBuildImage
-minikube image load quark-monolith-reactive:0.0.1-SNAPSHOT
+echo "           STAGE 1 - PREPARE NATIVE IMAGE         \n"
+CALL .\gradlew bootBuildImage
+echo "           STAGE 2 - UPLOAD NATIVE IMAGE TO MINIKUBE"
+CALL minikube image load quark-monolith-reactive:0.0.1-SNAPSHOT
+echo DONE!
+echo "           STAGE 3 - DELETE RUNNING POD(s)"
+CALL kubectl delete pod -l app=quark
